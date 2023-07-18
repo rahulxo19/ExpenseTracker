0792c5e5117dc15fc0fb2f83e9394ea21c72c69a
rahulxo19 <rahulchhikara2001@gmail.com>
2023-07-18 16:38:43 +0530

    adding expenseForm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 function App() {
@@ -30,11 +31,8 @@ function App() {
   
   return (
     <div>
-      <h2>Let's get started!</h2>
-
-          <Expenses items={expenses}></Expenses>
-
-
+      <NewExpense />
+      <Expenses items={expenses} />
     </div>
   );
 }
